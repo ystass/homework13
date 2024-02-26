89d7dff1ef8b1b388fec84d4c02ea890b0b70f87
ystass <stepanischevyy@mail.ru>
2024-02-26 17:04:25 +0400

    для созданных классов добавили инициализацию (__init__) для класса Category добавили два атрибута

--- src/category.py
+++ src/category.py
@@ -1,4 +1,16 @@
 class Category:
+    '''Класс <Категории>'''
     name: str
     description: str
     products: list
+
+    number_categories = 0
+    number_products = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.number_categories += 1
+        Category.number_products += len(self.products)
--- src/product.py
+++ src/product.py
@@ -1,5 +1,12 @@
 class Product:
+    '''Класс <Продукты>'''
     name: str
     description: str
     price: float
     quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
