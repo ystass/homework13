95e5fb22e60999e819738bc31172411f5a6d7d14
ystass <stepanischevyy@mail.ru>
2024-03-04 16:47:09 +0400

    для класса Product добавлен метод __add__

--- src/product.py
+++ src/product.py
@@ -14,6 +14,9 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
 
+    def __add__(self, other):
+        return self.price * self.quantity + other.price * other.quantity
+
     @classmethod
     def new_product(cls, name, description, price, quantity, products):
         for i in products:
